01617861c8f63a41ed21c9e810cbadfe21b5b7e1
Ben Neall <bneall@gmail.com>
2014-04-24 19:13:29 -0700

    Update gview.0.5.1.py

--- gview.0.5.1.py
+++ gview.0.5.1.py
@@ -3,7 +3,7 @@ import PySide.QtCore as QtCore
 import glob
 import os
 
-path = "/home/bneall/Pictures/test"
+path = "/usr/tmp/gViewThumbnails/usr/tmp/test/RUST_METAL"
 maxColumns = 4
 itemPad = 20
 itemSize = 210
@@ -11,47 +11,50 @@ itemSize = 210
 class GScene(QtGui.QGraphicsScene):
     def __init__(self):
         super(GScene, self).__init__()
+        #Background Color
         self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(95, 95, 95)))
-        self.menu = QtGui.QMenu()
-        importAction = self.menu.addAction("Import")
 
-    def printShit(self):
-        print 'shit'
+        #Context Menu
+        self.menu = QtGui.QMenu()
+        self.importAction = self.menu.addAction("Import")
 
     def contextMenuEvent(self, event):
-        try:
-            selectedAction = self.menu.exec_(event.screenPos())
-            selectedAction.triggered.connect(self.printShit)
-        except:
-            pass
-
+        self.menu.exec_(event.screenPos())
 
 class GRectItem(QtGui.QGraphicsRectItem):
     def __init__(self, image_path):
         super(GRectItem, self).__init__()
+        #Attributes
         self.setFlags(self.flags() | QtGui.QGraphicsItem.ItemIsSelectable | QtGui.QGraphicsItem.ItemIgnoresParentOpacity)
-        self.setRect(0,0,itemSize,itemSize)
+        self.setToolTip(image_path)
+
+        #Rect Settings
+        self.setRect(0, 0, itemSize, itemSize)
         self.rectBrush = QtGui.QBrush(QtGui.QColor(100, 100, 100))
-        self.rectPenBrush = QtGui.QBrush(QtGui.QColor(80, 80, 80))
+        self.rectPenBrush = QtGui.QBrush(QtGui.QColor(51, 51, 51))
         self.rectPen = QtGui.QPen(self.rectPenBrush, 1)
         self.setBrush(self.rectBrush)
         self.setPen(self.rectPen)
 
-        name = os.path.basename(image_path)
-        thumbTitleItem = QtGui.QGraphicsSimpleTextItem(name)
+        #Thumb Title Item
+        thumbTitle = os.path.basename(image_path)
+        thumbTitleItem = QtGui.QGraphicsSimpleTextItem(thumbTitle)
+        thumbTitleItem.setBrush(QtGui.QBrush(QtGui.QColor(200, 200, 200)))
         thumbTitleItem.setPos(0, itemSize)
         thumbTitleItem.setParentItem(self)
 
-        image = QtGui.QPixmap(image_path)
-        thumbImgItem = QtGui.QGraphicsPixmapItem(image)
-        width = thumbImgItem.pixmap().width()
-        height = thumbImgItem.pixmap().height()
-        hoffset = (itemSize-width)/2
-        voffset = (itemSize-height)/2
-        thumbImgItem.setPos(hoffset, voffset)
+        #Thumb Image Item
+        thumbImage = QtGui.QPixmap(image_path)
+        thumbImgItem = QtGui.QGraphicsPixmapItem(thumbImage)
+        thumbWidth = thumbImgItem.pixmap().width()
+        thumbHeight = thumbImgItem.pixmap().height()
+        thumbHOffset = (itemSize-thumbWidth)/2
+        thumbVOffset = (itemSize-thumbHeight)/2
+        thumbImgItem.setPos(thumbHOffset, thumbVOffset)
         thumbImgItem.setParentItem(self)
 
     def itemChange(self, change, value):
+        '''Detect Selection'''
         if change == QtGui.QGraphicsItem.ItemSelectedChange:
             if value == True:
                 self.setHighlite()
@@ -59,18 +62,13 @@ class GRectItem(QtGui.QGraphicsRectItem):
                 self.removeHighlite()
         return QtGui.QGraphicsItem.itemChange(self, change, value)
 
-    #def contextMenuEvent(self, event):
-        #menu = QtGui.QMenu()
-        #removeAction = menu.addAction("Remove")
-        #markAction = menu.addAction("Mark")
-        #selectedAction = menu.exec_(event.screenPos())
-
     def mousePressEvent(self, event):
+        '''Right Click Override'''
         if event.button() == QtCore.Qt.RightButton:
             pass
 
     def setHighlite(self):
-        highlitePenBrush = QtGui.QBrush(QtGui.QColor(221, 96, 0))
+        highlitePenBrush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
         highlitePen = QtGui.QPen(highlitePenBrush, 3)
         self.setPen(highlitePen)
 
@@ -92,54 +90,26 @@ class GView(QtGui.QDialog):
         self.gviewer = QtGui.QGraphicsView()
         self.gscene = GScene()
         self.gviewer.setInteractive(True)
-        #self.gscene = QtGui.QGraphicsScene()
         self.gviewer.setScene(self.gscene)
 
         self.searchLine = QtGui.QLineEdit()
         self.button = QtGui.QPushButton('Add')
-        self.button1 = QtGui.QPushButton('Filter')
 
         layout.addWidget(self.gviewer)
         layout.addWidget(self.searchLine)
         layout.addWidget(self.button)
-        layout.addWidget(self.button1)
 
         self.button.clicked.connect(self.createItems)
-        self.button1.clicked.connect(self.filterItems)
         self.searchLine.textChanged.connect(self.filterItems)
-
+        self.gscene.importAction.triggered.connect(self.importImages)
 
     def createItems(self):
         fileList =  sorted(glob.glob('%s/*' % path))
         for image in fileList:
-            self.createItem(image)
-
-    def createItem(self, image_path):
-        #Thumbnail Container
-        bgRectItem = GRectItem(image_path)
-        self.gscene.addItem(bgRectItem)
-
-        ##Thumbnail Image
-        #name = os.path.basename(image_path)
-        #image = QtGui.QPixmap(image_path)
-        #thumbImgItem = QtGui.QGraphicsPixmapItem(image)
-
-        ##Thumbnail Title
-        #thumbTitleItem = QtGui.QGraphicsSimpleTextItem(name)
-        #width = thumbImgItem.pixmap().width()
-        #height = thumbImgItem.pixmap().height()
-        #hoffset = (itemSize-width)/2
-        #voffset = (itemSize-height)/2
-        #thumbImgItem.setPos(hoffset, voffset)
-        #thumbTitleItem.setPos(0, itemSize)
-
-        ##Adding Thumbnail Group Item
-        #thumbGrpItem = self.gscene.createItemGroup([bgRectItem, thumbImgItem, thumbTitleItem])
-        #thumbGrpItem.setData(32, image_path)
-        #thumbGrpItem.setFlags(thumbGrpItem.flags() | QtGui.QGraphicsItem.ItemIsSelectable)
-
-        self.items += 1
-        self.setPositions(bgRectItem, itemSize, itemSize)
+            bgRectItem = GRectItem(image)
+            self.gscene.addItem(bgRectItem)
+            self.items += 1
+            self.setPositions(bgRectItem, itemSize, itemSize)
 
     def setPositions(self, item, hoffset, voffset):
         item.setPos(self.hSceneSize, self.vSceneSize)
@@ -151,37 +121,41 @@ class GView(QtGui.QDialog):
             self.hSceneSize = 0
 
     def filterItems(self):
+        #Reset
         self.items = 0
         self.hSceneSize = 0
         self.vSceneSize = 0
 
+        #List of Parent Items
         groupItems = []
         for item in self.gscene.items():
-            if not item.group():
+            if item.childItems():
                 groupItems.append(item)
                 item.setVisible(False)
                 item.setPos(0,0)
 
-        #Reverse group list
+        #Reverse Group List
         groupItems = groupItems[::-1]
 
+        #Display Only Items Matching Search
         for item in groupItems:
-            thumbItem = item.childItems()[1]
-            titleItem = item.childItems()[2]
-            hoffset = thumbItem.pixmap().width()
-            voffset = thumbItem.pixmap().height()
             searchText = self.searchLine.text()
+            titleItem = item.childItems()[0]
             thumbName = titleItem.text().split(".")[0]
             if searchText.lower() in thumbName.lower():
                 item.setVisible(True)
                 self.items += 1
                 self.setPositions(item, itemSize, itemSize)
 
+        #Resize Scene
+        boundingBox = self.gscene.itemsBoundingRect()
+        self.gscene.setSceneRect(boundingBox)
+
+        #Refresh UI
+        self.gscene.update()
         QtCore.QCoreApplication.processEvents()
 
-    def diagnostic(self):
-        print "group items", self.items
-        print "vertical scene size:", self.vSceneSize
-        print "horizontal scene size", self.hSceneSize
+    def importImages(self):
+        print 'fart'
 
 GView().exec_()
