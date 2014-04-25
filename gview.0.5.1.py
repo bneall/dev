56c546fcae53249c6ea5f877e1fc90a14b2c10c8
Ben Neall <bneall@gmail.com>
2014-04-25 05:00:57 -0700

    Update gview.0.5.1.py

--- gview.0.5.1.py
+++ gview.0.5.1.py
@@ -2,10 +2,12 @@ import PySide.QtGui as QtGui
 import PySide.QtCore as QtCore
 import glob
 import os
+import mari
 
-path = "/usr/tmp/gViewThumbnails/usr/tmp/test/RUST_METAL"
-maxColumns = 4
-itemPad = 20
+path = "C:\\temp\\gViewThumbnails\\C\\Users\\ben\\Documents\\Textures"
+maxColumns = 6
+itemHPad = 20
+itemVPad = 10
 itemSize = 210
 
 class GScene(QtGui.QGraphicsScene):
@@ -13,7 +15,6 @@ class GScene(QtGui.QGraphicsScene):
         super(GScene, self).__init__()
         #Background Color
         self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(95, 95, 95)))
-
         #Context Menu
         self.menu = QtGui.QMenu()
         self.importAction = self.menu.addAction("Import")
@@ -25,7 +26,7 @@ class GRectItem(QtGui.QGraphicsRectItem):
     def __init__(self, image_path):
         super(GRectItem, self).__init__()
         #Attributes
-        self.setFlags(self.flags() | QtGui.QGraphicsItem.ItemIsSelectable | QtGui.QGraphicsItem.ItemIgnoresParentOpacity)
+        self.setFlags(self.flags() | QtGui.QGraphicsItem.ItemIsSelectable)
         self.setToolTip(image_path)
 
         #Rect Settings
@@ -35,9 +36,18 @@ class GRectItem(QtGui.QGraphicsRectItem):
         self.rectPen = QtGui.QPen(self.rectPenBrush, 1)
         self.setBrush(self.rectBrush)
         self.setPen(self.rectPen)
+        
+        #Pad Rect Settings
+        self.setRect(0, 0, itemSize, itemSize)
+        padRect = QtGui.QGraphicsRectItem()
+        padRect.setPen(QtGui.QPen(self.rectPenBrush, 20))
+        padRect.setParentItem(self)
 
         #Thumb Title Item
         thumbTitle = os.path.basename(image_path)
+        #Title Size Check
+        if len(thumbTitle) >= 36:
+            thumbTitle = thumbTitle[:34]+'...'
         thumbTitleItem = QtGui.QGraphicsSimpleTextItem(thumbTitle)
         thumbTitleItem.setBrush(QtGui.QBrush(QtGui.QColor(200, 200, 200)))
         thumbTitleItem.setPos(0, itemSize)
@@ -75,32 +85,40 @@ class GRectItem(QtGui.QGraphicsRectItem):
     def removeHighlite(self):
         self.setPen(self.rectPen)
 
-class GView(QtGui.QDialog):
+class GView(QtGui.QWidget):
     vSceneSize = 0
     hSceneSize = 0
     items = 0
-
     def __init__(self):
         super(GView, self).__init__()
         self.setMinimumSize(1000,1000)
 
         layout = QtGui.QVBoxLayout()
+        hlayout = QtGui.QHBoxLayout()
         self.setLayout(layout)
 
         self.gviewer = QtGui.QGraphicsView()
         self.gscene = GScene()
         self.gviewer.setInteractive(True)
         self.gviewer.setScene(self.gscene)
+        self.gviewer.setAlignment( QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft )
 
         self.searchLine = QtGui.QLineEdit()
+        self.columnSpin = QtGui.QSpinBox()
         self.button = QtGui.QPushButton('Add')
+        self.button2 = QtGui.QPushButton('Fit')
 
         layout.addWidget(self.gviewer)
-        layout.addWidget(self.searchLine)
-        layout.addWidget(self.button)
+        layout.addLayout(hlayout)
+        hlayout.addWidget(self.button2)
+        hlayout.addWidget(self.searchLine)
+        hlayout.addWidget(self.columnSpin)
+        hlayout.addWidget(self.button)
 
         self.button.clicked.connect(self.createItems)
-        self.searchLine.textChanged.connect(self.filterItems)
+        self.button2.clicked.connect(self.fitColumnsMari)
+        self.searchLine.textChanged.connect(self.sortItems)
+        self.columnSpin.valueChanged.connect(self.setColumns)
         self.gscene.importAction.triggered.connect(self.importImages)
 
     def createItems(self):
@@ -109,18 +127,30 @@ class GView(QtGui.QDialog):
             bgRectItem = GRectItem(image)
             self.gscene.addItem(bgRectItem)
             self.items += 1
-            self.setPositions(bgRectItem, itemSize, itemSize)
+            self.setPositions(bgRectItem)
+        self.fitColumnsMari()
 
-    def setPositions(self, item, hoffset, voffset):
+    def setPositions(self, item):
         item.setPos(self.hSceneSize, self.vSceneSize)
-
         if self.items % maxColumns:
-            self.hSceneSize += hoffset+itemPad
+            self.hSceneSize += itemSize+itemVPad
         else:
-            self.vSceneSize += voffset+itemPad
+            self.vSceneSize += itemSize+itemHPad
             self.hSceneSize = 0
-
-    def filterItems(self):
+            
+    def setColumns(self):
+        global maxColumns
+        maxColumns = self.columnSpin.value()
+        self.sortItems()
+    
+    def fitColumnsMari(self):
+        available_area = mari.app.canvasWidth()
+        global maxColumns
+        maxColumns = int(round(available_area/(itemSize+itemHPad)))
+        self.columnSpin.setValue(maxColumns)
+        self.sortItems()
+    
+    def sortItems(self):
         #Reset
         self.items = 0
         self.hSceneSize = 0
@@ -134,22 +164,23 @@ class GView(QtGui.QDialog):
                 item.setVisible(False)
                 item.setPos(0,0)
 
-        #Reverse Group List
+        ##Reverse Group List
         groupItems = groupItems[::-1]
-
         #Display Only Items Matching Search
         for item in groupItems:
             searchText = self.searchLine.text()
-            titleItem = item.childItems()[0]
+            titleItem = item.childItems()[1]
             thumbName = titleItem.text().split(".")[0]
             if searchText.lower() in thumbName.lower():
                 item.setVisible(True)
                 self.items += 1
-                self.setPositions(item, itemSize, itemSize)
+                self.setPositions(item)
 
         #Resize Scene
-        boundingBox = self.gscene.itemsBoundingRect()
-        self.gscene.setSceneRect(boundingBox)
+        autoRect = self.gscene.itemsBoundingRect()
+        self.gscene.setSceneRect(autoRect)
+        ##Scroll to top
+        self.gviewer.ensureVisible(0.0,0.0,0.0,0.0)
 
         #Refresh UI
         self.gscene.update()
@@ -158,4 +189,7 @@ class GView(QtGui.QDialog):
     def importImages(self):
         print 'fart'
 
-GView().exec_()
+gviewWidget = GView()
+mari.app.addTab('gView', gviewWidget)
+
+# GView().exec_()
