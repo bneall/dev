0bef78764dc34b826acc17dc3549cbb550a07df2
Ben Neall <bneall@gmail.com>
2014-05-21 17:03:14 -0700

    Delete init_gView.py

--- init_gView.py
+++
@@ -1,9 +0,0 @@
-try:
-  import gView.gViewer as gViewer
-  gViewerWidget = gViewer.GView()
-  mari.app.addTab('gView', gViewerWidget)
-  print '##----------------------------'
-  print 'gView 0.5.3 Loaded Succesfuly '
-  print '##----------------------------'
-except:
-    print 'Unable to load gView, please check install'
