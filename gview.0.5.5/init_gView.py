70fa1474fde2d0516e2795e7f804f1ad8506f7ff
Ben Neall <bneall@gmail.com>
2014-05-29 14:00:32 -0700

    Create init_gView.py

---
+++ gview.0.5.5/init_gView.py
@@ -0,0 +1,9 @@
+#try:
+import gView.gViewer as gViewer
+gViewerWidget = gViewer.GView()
+mari.app.addTab('gView', gViewerWidget)
+print '##----------------------------'
+print 'gView 0.5.5 Loaded Succesfuly '
+print '##----------------------------'
+#except:
+    #print 'Unable to load gView, please check install'
